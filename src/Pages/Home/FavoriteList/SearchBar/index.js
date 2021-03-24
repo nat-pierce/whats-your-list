65e52b6dffc7d1e00cca4a5ff2c127666e59892b
nat <nat.r.pierce@gmail.com>
2021-03-23 21:01:10 -0400

    debounced search input

---
+++ src/Pages/Home/FavoriteList/SearchBar/index.js
@@ -0,0 +1,40 @@
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useState, useEffect, useCallback } from 'react';
+import TextField from '@material-ui/core/TextField';
+import debounce from 'lodash.debounce';
+import { getSearchOptionsAsync } from './SearchUtilities';
+
+export default function SearchBar() {
+    const [inputValue, setInputValue] = useState('');
+
+    const [options, setOptions] = useState([]);
+
+    const onInputChange = (e, value) => {
+        setInputValue(value);
+    }
+
+    // https://stackoverflow.com/questions/40811535/delay-suggestion-time-material-ui-autocomplete-component
+    const getOptionsDelayed = useCallback(
+        debounce((text, callback) => {
+            setOptions([]);
+            getSearchOptionsAsync(text).then(callback);
+        }, 200),
+        []
+    );
+
+    useEffect(() => {
+        getOptionsDelayed(inputValue, (filteredOptions) => {
+          setOptions(filteredOptions);
+        });
+      }, [inputValue, getOptionsDelayed]);
+
+    return (
+        <Autocomplete 
+            options={options}
+            onInputChange={onInputChange}
+            loading={options.length === 0}
+            getOptionLabel={(option) => option.title}
+            renderInput={(params) => <TextField {...params} label="Search" color="secondary" variant="outlined" />}
+        />
+    );
+}
