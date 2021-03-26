7a09a5c879811ae5e402630092212c147021ce1e
nat <nat.r.pierce@gmail.com>
2021-03-26 19:08:44 -0400

    search behavior

--- src/Pages/Home/SearchBar.js
+++ src/Pages/Home/SearchBar.js
@@ -3,21 +3,24 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import { useState, useEffect, useCallback } from 'react';
 import TextField from '@material-ui/core/TextField';
 import debounce from 'lodash.debounce';
-import { getSearchOptionsAsync } from './SearchUtilities';
-import { searchMovieApi } from '../../../ApiUtilities';
+import { searchMovieApi } from '../../ApiUtilities';
 
 export default function SearchBar() {
     const [inputValue, setInputValue] = useState('');
     const [options, setOptions] = useState([]);
+    const [reactKey, setReactKey] = useState(0);
     
     console.log('o', options);
 
     const onChooseMovie = (e, value) => {
         console.log(e, value);
+        setReactKey(reactKey+1);
+        setInputValue('');
+        setOptions([]);
     };
 
     const getOptionSelected = (option, value) => {
-        return option.imdbID === value.imdbID
+        // return option.imdbID === value.imdbID
     };
 
     const onInputChange = (e, value) => {
@@ -41,16 +44,19 @@ export default function SearchBar() {
 
     return (
         <Autocomplete 
+            key={reactKey}
             className='search'
+            multiple={false}
             options={options}
             onChange={onChooseMovie}
             onInputChange={onInputChange}
-            loading={options.length === 0}
+            loading={(options.length === 0) && (inputValue !== '')}
+            noOptionsText={'Search for your favorite movies by title'}
             getOptionSelected={getOptionSelected}
             // disable filtering on client side
             filterOptions={(option) => option}
             getOptionLabel={(option) => option.Title}
-            renderInput={(params) => <TextField {...params} label="Search by title" color="secondary" variant="outlined" />}
+            renderInput={(params) => <TextField {...params} label="Search" color="secondary" variant="outlined" />}
         />
     );
 }
