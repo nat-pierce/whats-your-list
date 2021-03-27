bc667721b501e5e1c0b7a61d3fb93b3a404e0b9c
nat <nat.r.pierce@gmail.com>
2021-03-27 18:34:27 -0400

    get by title when too many results

--- src/ApiUtilities.js
+++ src/ApiUtilities.js
@@ -1,6 +1,6 @@
 export const searchMovieApi = (title, callback) => {
-    if (!title || !callback) {
-        return [];
+    if (!title) {
+        return callback([]);
     }
 
     const encodedTitle = encodeURIComponent(title);
@@ -15,13 +15,41 @@ export const searchMovieApi = (title, callback) => {
     })
     .then(response => response.json())
     .then(data => {
-        if (!data.Error) {
+        if (data && !data.Error) {
             callback(data.Search)
+        } else if (data.Error === 'Too many results.') {
+            getMovieByTitle(title, callback);
         }
     })
     .catch(err => {
         console.error(err);
     });
+}
+
+const getMovieByTitle = (title, callback) => {
+    if (!title || !callback) {
+        return [];
+    }
+
+    const encodedTitle = encodeURIComponent(title);
+    const url = `https://movie-database-imdb-alternative.p.rapidapi.com/?t=${encodedTitle}&r=json&type=movie`;
+
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": process.env.REACT_APP_MOVIE_API_KEY,
+            "x-rapidapi-host": process.env.REACT_APP_MOVIE_API_HOST
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data && !data.Error) {
+            callback([data])
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    })
 }
 
 export const getMovieMetadataApi = (imdbID) => {
