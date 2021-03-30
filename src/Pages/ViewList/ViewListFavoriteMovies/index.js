c6232b1b698b8e06ea3033f643f366ab7764d311
nat <nat.r.pierce@gmail.com>
2021-03-29 21:26:27 -0400

    show my rank

--- src/Pages/ViewList/ViewListFavoriteMovies/index.js
+++ src/Pages/ViewList/ViewListFavoriteMovies/index.js
@@ -1,18 +1,36 @@
-export default function ViewListFavoriteMovies({ viewListMovies }) {
+import { memo, useContext } from "react";
+import AppContext from "../../../AppContext";
+import './ViewListFavoriteMovies.scss';
+
+const ViewListFavoriteMovies = memo(({ viewListMovies, favoriteMovies }) => {
     return (
-        <div className='favorite-list'>
-            {viewListMovies && viewListMovies.map((movie, i) => (
-                <div className='tile' key={movie.imdbID}>
-                    <div className='rank'>#{i+1}</div>
-                    {movie.Poster !== 'N/A' && 
-                        <img 
-                            className='poster' 
-                            src={movie.Poster} 
-                            alt='Movie poster' />
-                    }
-                    <div>{movie.Title} ({movie.Year})</div>
-                </div>
-            ))}
+        <div className='favorite-list view-list-favorite-movies'>
+            {viewListMovies && viewListMovies.map((movie, i) => {
+                const myRank = favoriteMovies.findIndex(m => m.imdbID === movie.imdbID) + 1;
+
+                return (
+                    <div className='tile' key={movie.imdbID}>
+                        <div className='rank'>#{i+1}</div>
+                        {movie.Poster !== 'N/A' && 
+                            <img 
+                                className='poster' 
+                                src={movie.Poster} 
+                                alt='Movie poster' />
+                        }
+                        <div>{movie.Title} ({movie.Year})</div>
+                        {myRank > 0 && <div className='my-rank'>My rank: {myRank}</div>}
+                    </div>
+                );
+            })}
         </div>
     );
+});
+
+export default function ConnectedViewListFavoriteMovies(props) {
+    const { state } = useContext(AppContext);
+    const favoriteMovies = state.user
+        ? state.favoriteMovies
+        : [];
+
+    return <ViewListFavoriteMovies {...props} favoriteMovies={favoriteMovies} />
 }
--- src/Pages/ViewList/ViewListProfile/ViewListFriends/index.js
+++ src/Pages/ViewList/ViewListProfile/ViewListFriends/index.js
@@ -1,35 +1,57 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState, useContext, memo } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { getFriendsInfo } from '../../../../FirebaseFunctions';
 import './ViewListFriends.scss';
 import Modal from '../../../../CommonComponents/Modal';
+import AppContext from '../../../../AppContext';
+import Button from '@material-ui/core/Button';
 
-export default function ViewListFriends({ viewId, isModalOpen, onCloseModal }) {
+const ViewListFriends = memo(({ uid, viewId, isModalOpen, onCloseModal, addFriend }) => {
     const [viewListFriends, setViewListFriends] = useState([]);
+    const [sentRequests, setSentRequests] = useState([]);
+
+    const onClickAddFriend = (id) => {
+        setSentRequests([...sentRequests, id]);
+        addFriend(id);
+    }
 
     useEffect(() => {
         if (viewListFriends.length) { return };
 
         getFriendsInfo(viewId).then(({ result }) => {
-            setViewListFriends([...result, ...result, ...result, ...result, ...result]);
+            const filteredFriends = result.filter(f => f.uid !== uid);
+            setViewListFriends(filteredFriends);
         })
-    }, [viewListFriends, setViewListFriends, viewId]);
+    }, [viewListFriends, setViewListFriends, viewId, uid]);
     
     return (
         <Modal className='view-friends-modal' modalTitle={`View friends (${viewListFriends.length})`} isOpen={isModalOpen} onCloseModal={onCloseModal}>
             <div className='view-list-friends'>
-                { viewListFriends.length &&
-                    viewListFriends.map(friend => (
-                        <div className='friend' key={friend.uid}>
-                            <div className='profile-info'>
-                                <Avatar className='profile-pic' src={friend.profilePicUrl} alt='Profile pic' />
-                                <div className='name'>{friend.name}</div>
-                            </div>
+                { viewListFriends.map(friend => (
+                    <div className='friend' key={friend.uid}>
+                        <div className='profile-info'>
+                            <Avatar className='profile-pic' src={friend.profilePicUrl} alt='Profile pic' />
+                            <div className='name'>{friend.name}</div>
                         </div>
-                    ))
-                }
+                        {sentRequests.includes(friend.uid)
+                            ? <div>Sent request</div>
+                            : (
+                                <Button className='add-button' onClick={() => onClickAddFriend(friend.uid)}>
+                                    Add
+                                </Button>
+                            )
+                        }
+                    </div>
+                ))}
             </div>
         </Modal>
     );
+});
+
+export default function ConnectedViewListFriends(props) {
+    const { state, actions } = useContext(AppContext);
+    const { addFriend } = actions;
+    const uid = state.user.uid;
+
+    return <ViewListFriends addFriend={addFriend} uid={uid} {...props} />;
 }
