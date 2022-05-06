ea4ac522078e01dcb1adab68018495bf0e92c530
nat <nat.r.pierce@gmail.com>
2022-05-05 21:00:06 -0400

    adding icon to popup

--- src/CommonComponents/AddToHomeScreenPopup/index.js
+++ src/CommonComponents/AddToHomeScreenPopup/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './AddToHomeScreenPopup.scss';
+import appIcon from '../../Resources/Images/appIcon.png';
 
 export default function AddToHomeScreenPopUp() {
     return (
         <div className='add-to-home-popup'>
             <span className='popup-arrow' />
+            <img className='app-icon' src={appIcon} alt='app-icon' />
             Add to home screen
         </div>
     );
