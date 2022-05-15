465db2f26e16a4d396f373e5990f87c399b7c146
nat <nat.r.pierce@gmail.com>
2022-05-15 16:23:16 -0400

    prevent friend retrieval in local

--- src/FirebaseFunctions.js
+++ src/FirebaseFunctions.js
@@ -1,15 +1,20 @@
 export const getFriendsInfo = async (userId) => {
-    const baseUrl = process.env.NODE_ENV === 'production'
-        ? 'https://us-central1-whats-your-list.cloudfunctions.net'
-        : 'http://localhost:5001/whats-your-list/us-central1';
+    const isProdEnv = process.env.NODE_ENV === 'production';
+    const isLocal = window.location.hostname === 'localhost';
+    
+    if (isProdEnv && !isLocal) {
+        const baseUrl = process.env.NODE_ENV === 'production'
+            ? 'https://us-central1-whats-your-list.cloudfunctions.net'
+            : 'http://localhost:5001/whats-your-list/us-central1';
 
-    const url = `${baseUrl}/getFriendsInfo?userId=${userId}`;
+        const url = `${baseUrl}/getFriendsInfo?userId=${userId}`;
 
-    return fetch(url, {
-        "method": "GET"
-    })
-    .then(response => response.json())
-    .catch(err => {
-        console.error(err);
-    });
+        return fetch(url, {
+            "method": "GET"
+        })
+        .then(response => response.json())
+        .catch(err => {
+            console.error(err);
+        });
+    }
 }
