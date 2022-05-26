495fe009eb1e278687e4e7195ea8b7feacc0e5b9
nat <nat.r.pierce@gmail.com>
2022-05-26 08:03:03 -0400

    tile styles

--- src/CommonComponents/MovieTile/index.js
+++ src/CommonComponents/MovieTile/index.js
@@ -9,18 +9,32 @@ export default function MovieTile({
     children
 }) {
     const rankDisplay = rank && <div className='rank'>#{rank}</div>;
+    const isTopTen = rank <= 10;
+    const isSingleDigit = rank <= 9;
+    const isDoubleDigit = rank >= 10 && rank <= 99;
+    const isTripleDigit = rank >= 100;
+
+    const classNames = [
+        'movie-tile',
+        isTopTen ? 'top-ten' : '',
+        isSingleDigit ? 'single' : '',
+        isDoubleDigit ? 'double' : '',
+        isTripleDigit ? 'triple' : ''
+    ];
 
     return (
-        <div className='movie-tile'>
-            {dragHandleProps 
-                ? ( 
-                    <div className='drag-handle'{...dragHandleProps}>
-                        <DragHandleIcon />
-                        {rankDisplay}
-                    </div>
-                )
-                : rankDisplay
-            }
+        <div className={classNames.join(' ')}>
+            <div className='left-content'>
+                {dragHandleProps 
+                    ? ( 
+                        <div className='drag-handle'{...dragHandleProps}>
+                            <DragHandleIcon />
+                            {rankDisplay}
+                        </div>
+                    )
+                    : rankDisplay
+                }
+            </div>
             <img className='poster' src={movie.Poster} alt='Movie poster' />
             <div className='title'>{movie.Title} ({movie.Year})</div>
             {children}
