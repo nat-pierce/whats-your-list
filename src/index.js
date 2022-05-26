59cfc39b1c664ec089f964c98f6cb7f8563801c3
nat <nat.r.pierce@gmail.com>
2022-05-26 18:57:10 -0400

    preload fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import firebase, { FirebaseContext } from './Firebase';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import './Fonts.css';
 
 // import reportWebVitals from './reportWebVitals';
 
