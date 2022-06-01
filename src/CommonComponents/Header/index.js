06c6e7cb86468c3c6102b2f29118f59b46c55f53
nat-pierce <nat.r.pierce@gmail.com>
2022-06-01 10:23:21 -0400

    refresh button

--- src/CommonComponents/Header/index.js
+++ src/CommonComponents/Header/index.js
@@ -11,11 +11,14 @@ import SettingsModal from './Settings';
 import { getHasAttemptedSignIn, getIsSignedIn } from '../../AppSelectors';
 import { useLocation } from 'react-router-dom';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import RefreshIcon from '@material-ui/icons/Refresh';
 import Guide from '../Guide';
+import { getIsOnline } from '../../Utilities/EnvironmentUtilities';
 
 const Header = memo(({ 
     shouldShowOverlay, 
     isSignedIn,
+    isOnline,
     shouldUseAboutIntro
 }) => {
     const history = useHistory();
@@ -37,6 +40,10 @@ const Header = memo(({
         history.push(ROUTES.Login);
     };
 
+    const onClickRefresh = () => {
+        history.go(0);
+    }
+
     // Splash logo / Logo-button in top-left of header
     const canClickHeader = !shouldShowOverlay && (pathname !== ROUTES.Login) && (pathname !== ROUTES.Home); 
     const logo = (
@@ -54,21 +61,38 @@ const Header = memo(({
         </div>
     );
 
-    // Action-button in top-right of header
-    let actionButton = null;
-    if (!shouldShowOverlay && (pathname !== ROUTES.Login)) {
-        actionButton = isSignedIn
-            ? (
-                <IconButton className="action-button settings-icon" onClick={() => setIsSettingsModalOpen(true)}>
-                    <SettingsIcon />
-                </IconButton>
-            )
-            : (
-                <Button className='action-button create-list' color="primary" variant="contained" onClick={onClickLogin}>
-                    Create my list
-                </Button>
-            );
+    //#region Action-button in top-right of header
+    // 1: if we're in loading state, don't show anything
+    // 2: otherwise, if we're offline (even at login), show a refresh button
+    // 3: otherwise, if we're at the login page, don't show anything
+    // 4: otherwise, if we're not signed in (viewing list anonymously), show a button to go login
+    // 5: otherwise, we can show the settings
+    let actionButton, canShowSettings;
+    if (shouldShowOverlay) {
+        actionButton = null;
+    } else if (!isOnline) {
+        actionButton = (
+            <Button className="action-button refresh-button" color="primary" variant="contained" onClick={onClickRefresh}>
+                <RefreshIcon /> Refresh
+            </Button>
+        );
+    } else if (pathname === ROUTES.Login) {
+        actionButton = null;
+    } else if (!isSignedIn) {
+        actionButton = (
+            <Button className='action-button create-list' onClick={onClickLogin}>
+                Create my list
+            </Button>
+        );
+    } else {
+        canShowSettings = true;
+        actionButton = (
+            <IconButton className="action-button settings-icon" onClick={() => setIsSettingsModalOpen(true)}>
+                <SettingsIcon />
+            </IconButton>
+        );
     }
+    //#endregion
 
     return (
         <>
@@ -76,7 +100,7 @@ const Header = memo(({
                 {logo}
                 {actionButton}
             </div>
-            {isSettingsModalOpen && !shouldShowOverlay && (pathname !== ROUTES.Login) &&
+            {isSettingsModalOpen && canShowSettings &&
                 <SettingsModal 
                     shouldUseAboutIntro={shouldUseAboutIntro}
                     onClose={() => {
@@ -100,10 +124,12 @@ export default function ConnectedHeader() {
     const shouldShowOverlay = !getHasAttemptedSignIn(state);
     const isSignedIn = getIsSignedIn(state);
     const shouldUseAboutIntro = !localStorage.getItem(LOCAL_STORAGE_ABOUT_INTRO);
+    const isOnline = getIsOnline();
 
     return <Header 
         shouldShowOverlay={shouldShowOverlay} 
         isSignedIn={isSignedIn}
         shouldUseAboutIntro={shouldUseAboutIntro}
+        isOnline={isOnline}
     />;
 }
