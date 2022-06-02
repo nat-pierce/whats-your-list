20a0cac787258fb3982a862b73417fb6955eda21
nat-pierce <nat.r.pierce@gmail.com>
2022-06-02 15:55:49 -0400

    style fixes

--- src/Pages/Friends/FindFriends/index.js
+++ src/Pages/Friends/FindFriends/index.js
@@ -73,7 +73,7 @@ const FindFriends = memo(({ uid, addFriend, friends }) => {
 
     return (
         <div className='find-friends'>
-            <div className='instructions'>Find new friends by searching their first or last names</div>
+            <div className='instructions'>Find new friends using their first or last name</div>
             <div className='upper'>
                 <TextField 
                     className='search-box'
